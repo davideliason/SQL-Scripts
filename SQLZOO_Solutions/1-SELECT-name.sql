66ce7c2c92432f9f87f6cf001c1fa93c5745c7e8
David Eliason <davethemaker@gmail.com>
2019-12-31 14:29:36 -0800

    solution for #11 using CONCAT as part of query

--- SQLZOO_Solutions/1-SELECT-name.sql
+++ SQLZOO_Solutions/1-SELECT-name.sql
@@ -111,5 +111,13 @@ Find the country where the name is the capital city.
   FROM world
  WHERE name LIKE capital
 
+#12
+/*
+The capital of Mexico is Mexico City. Show all the countries where the capital has the country together with the word "City".
 
+Find the country where the capital is the country plus "City".
+*/
 
+SELECT name
+  FROM world
+ WHERE capital LIKE concat(name, ' City')
