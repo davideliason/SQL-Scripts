9c7d193185b0a83cce7d34b1aea06ab806129a6f
David Eliason <davethemaker@gmail.com>
2019-12-31 14:44:31 -0800

    solution #15 using REPLACE and wildcard

--- SQLZOO_Solutions/1-SELECT-name.sql
+++ SQLZOO_Solutions/1-SELECT-name.sql
@@ -143,3 +143,16 @@ You should include Mexico City as it is longer than Mexico. You should not inclu
 SELECT capital, name 
   FROM world
  WHERE capital LIKE concat(name, '%') AND capital <> name
+
+ #15
+ /*
+For Monaco-Ville the name is Monaco and the extension is -Ville.
+
+Show the name and the extension where the capital is an extension of name of the country.
+
+You can use the SQL function REPLACE.
+ */
+ 
+ SELECT name, REPLACE(capital, name, '')
+  FROM world
+ WHERE capital LIKE concat(name, '%') AND capital <> name
