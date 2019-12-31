b320575054a3a4b5739444ac9247e20052d80b1f
David Eliason <davethemaker@gmail.com>
2019-12-31 14:22:35 -0800

    solution for #9 using underscore

--- SQLZOO_Solutions/1-SELECT-name.sql
+++ SQLZOO_Solutions/1-SELECT-name.sql
@@ -79,3 +79,13 @@ Find the countries that have "t" as the second character.
 SELECT name FROM world
  WHERE name LIKE '_t%'
 ORDER BY name
+
+#9
+/*
+Lesotho and Moldova both have two o characters separated by two other characters.
+
+Find the countries that have two "o" characters separated by two others.
+*/
+
+SELECT name FROM world
+ WHERE name LIKE '%o__o%'
