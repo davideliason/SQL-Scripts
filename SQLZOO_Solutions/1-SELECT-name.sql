7d31cbc8e85bb2afafe6d9e695db65c73770a2f1
David Eliason <davethemaker@gmail.com>
2019-12-31 12:36:21 -0800

    #3 solution

--- SQLZOO_Solutions/1-SELECT-name.sql
+++ SQLZOO_Solutions/1-SELECT-name.sql
@@ -16,3 +16,13 @@ Find the countries that end with y
 
 SELECT name FROM world
   WHERE name LIKE '%y'
+
+#3
+/*
+Luxembourg has an x - so does one other country. List them both.
+
+Find the countries that contain the letter x
+*/
+
+SELECT name FROM world
+  WHERE name LIKE '%x%'
