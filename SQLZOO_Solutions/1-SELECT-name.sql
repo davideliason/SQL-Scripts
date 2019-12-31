ad2b98c99358edbd77bffba0154e17027fdca7a5
David Eliason <davethemaker@gmail.com>
2019-12-31 14:23:35 -0800

    use of underscore for four wildcards

--- SQLZOO_Solutions/1-SELECT-name.sql
+++ SQLZOO_Solutions/1-SELECT-name.sql
@@ -89,3 +89,15 @@ Find the countries that have two "o" characters separated by two others.
 
 SELECT name FROM world
  WHERE name LIKE '%o__o%'
+
+ #10
+ /*
+Cuba and Togo have four characters names.
+
+Find the countries that have exactly four characters.
+ */
+
+ SELECT name FROM world
+ WHERE name LIKE '____'
+
+
