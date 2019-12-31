899dd07654035d3c36aa77d26cca2c198941d1ad
David Eliason <davethemaker@gmail.com>
2019-12-31 14:17:03 -0800

    #6 solution wildcard %

--- SQLZOO_Solutions/1-SELECT-name.sql
+++ SQLZOO_Solutions/1-SELECT-name.sql
@@ -46,3 +46,13 @@ Find the countries that start with C and end with ia
 SELECT name FROM world
   WHERE name LIKE 'C%ia'
 
+#6
+/*
+Greece has a double e - who has a double o?
+
+Find the country that has oo in the name
+*/
+
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
+  
