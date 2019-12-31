8d21f570585f518c032132b4495f65015bc36c9e
David Eliason <davethemaker@gmail.com>
2019-12-31 14:26:41 -0800

    add solutino using LIKE to compare two values

--- SQLZOO_Solutions/1-SELECT-name.sql
+++ SQLZOO_Solutions/1-SELECT-name.sql
@@ -100,4 +100,16 @@ Find the countries that have exactly four characters.
  SELECT name FROM world
  WHERE name LIKE '____'
 
+ #11
+ /*
+The capital of Luxembourg is Luxembourg. Show all the countries where the capital is the same as the name of the country
+
+Find the country where the name is the capital city.
+ */
+ 
+ SELECT name
+  FROM world
+ WHERE name LIKE capital
+
+
 
