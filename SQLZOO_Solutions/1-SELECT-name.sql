47e4540c12386e1c0f035b1710ede27d42e3efda
David Eliason <davethemaker@gmail.com>
2019-12-31 14:18:25 -0800

    #7 solution wildcard

--- SQLZOO_Solutions/1-SELECT-name.sql
+++ SQLZOO_Solutions/1-SELECT-name.sql
@@ -55,4 +55,13 @@ Find the country that has oo in the name
 
 SELECT name FROM world
   WHERE name LIKE '%oo%'
-  
+
+#7
+/*
+Bahamas has three a - who else?
+
+Find the countries that have three or more a in the name
+*/
+
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
