8379be02b8f789ba0b7c4fa10337019f69b0c1de
David Eliason <davethemaker@gmail.com>
2019-12-31 12:31:13 -0800

    #2 solution

--- SQLZOO_Solutions/0-Select-basics.sql
+++ SQLZOO_Solutions/0-Select-basics.sql
@@ -6,3 +6,13 @@ Modify it to show the population of Germany
 
 SELECT population FROM world
   WHERE name = 'Germany'
+
+#2
+/*
+Checking a list The word IN allows us to check if an item is in a list. The example shows the name and population for the countries 'Brazil', 'Russia', 'India' and 'China'.
+
+Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+*/
+
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway' and 'Denmark');
