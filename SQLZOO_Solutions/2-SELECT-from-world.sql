e41f8a1a0ab48e6053a7b51b401eec4e6c6ce0ac
David Eliason <davethemaker@gmail.com>
2020-01-06 10:15:21 -0800

    query based on continent

--- SQLZOO_Solutions/2-SELECT-from-world.sql
+++ SQLZOO_Solutions/2-SELECT-from-world.sql
@@ -13,3 +13,12 @@ Give the name and the per capita GDP for those countries with a population of at
 SELECT name, gdp/population
 FROM world
 WHERE population >= 200000000
+
+#4
+/*
+Show the name and population in millions for the countries of the continent 'South America'. Divide the population by 1000000 to get population in millions.
+*/
+SELECT name, population/1000000
+FROM world
+WHERE continent = 'South America'
+
