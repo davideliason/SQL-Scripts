15476e9fa4cb2e66aaff174654f2fd63cf62025d
David Eliason <davethemaker@gmail.com>
2020-01-06 10:13:30 -0800

    #3 per capital gdp

--- SQLZOO_Solutions/2-SELECT-from-world.sql
+++ SQLZOO_Solutions/2-SELECT-from-world.sql
@@ -5,3 +5,11 @@ How to use WHERE to filter records. Show the name for the countries that have a 
 */
 SELECT name FROM world
 WHERE population >= 200000000
+
+#3
+/*
+Give the name and the per capita GDP for those countries with a population of at least 200 million.
+*/
+SELECT name, gdp/population
+FROM world
+WHERE population >= 200000000
