40e7860476f9e4a49fd8f29ed6818a7c44380da7
David Eliason <davethemaker@gmail.com>
2020-01-06 10:57:23 -0800

    answer using AND for query

--- SQLZOO_Solutions/3-SELECT-from-nobel.sql
+++ SQLZOO_Solutions/3-SELECT-from-nobel.sql
@@ -7,3 +7,17 @@ Change the query shown so that it displays Nobel prizes for 1950.
 SELECT yr, subject, winner
   FROM nobel
  WHERE yr = 1950
+
+#2
+/*
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+*/
+
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+
